2418c316485b047d1e1d13393dda25ffacc1e219
Jose Juan Parra Alcolea <josejuan.parraalcolea@192-168-1-7.tpgi.com.au>
2018-03-13 07:46:55 +1100

    second comit subtrac values click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+
+
 
 let score = {
   X: 0,
@@ -6,39 +9,73 @@ let score = {
 
 
 
-//let playgame = [ _ , _ , _ , _ , _ , _ , _ , _ , _ ];
-let wincombinations = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 5, 9],
+let gameOutcome = [ '_' , '_' , '_' , '_' , '_' , '_' , '_' , '_' , '_' ];
+
+let winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  [3, 6, 9],
-  [3, 5, 7]
+  [2, 4, 6]
 ];
 let playerTurn = '';
 let turn = 0;
+let chosenArray = [];
+
+
+
+let playGame = function () {
+  parseInt(this.id);
+  console.log(this.id.parseInt());
+}
+$('.square').on('click', playGame);
+
+
 
 // PLAYER TURN//////////////////////////////////////////
 
-const newTurn = function (turn) {
-  if (turn % 2 === 0) {
-    playerTurn = 'O';     //Player 2 goes
+// const newTurn = function (turn) {
+//   if (turn % 2 === 0) {
+//     playerTurn = 'O';     //Player 2 goes
+//
+//     console.log('Player 2');
+//   } else if (turn % 2 !== 0) {
+//     playerTurn = 'X';    //Player 1 goes
+//
+//     console.log('Player 1');
+//   }
+//   return turn += 1
+//   console.log('we are on turn number ' +turn);
+//   console.log(playerTurn);
+// };
+//
+// ////CHOOSE THE MOVEMENT/////////////////////////////////////////
+//
+// $("html").click(function () {    /////CLICK ANYWHERE ON THE SCREEN
+//   turn = newTurn(turn)
+// });
 
-    console.log('Player 2');
-  } else if (turn % 2 !== 0) {
-    playerTurn = 'X';    //Player 1 goes
+////////////////////////////////////////////////////////TEST//////
 
-    console.log('Player 1');
-  }
-  return turn += 1
-  console.log('we are on turn number ' +turn);
-  console.log(playerTurn);
-};
-
-$("html").click(function () {    /////CLICK ANYWHERE ON THE SCREEN
-  turn = newTurn(turn)
+// let play = function (chosenArray) {
+//   if (turn < 9) {
+// console.log(chosenArray);
+//     for (var i = 0; i < winCombinations.length; i++) {
+//
+//       matchedMoves = 0;
+//       // console.log('hola');
+//       for (var j = 0; j < chosenArray.length; j++) {
+//
+//         if (winCombinations[i][j] === chosenArray[j]) {
+// console.log('is');
+//         return matchedMoves += 1
+//         }
+//       }
+//     }
+//   }
+// };
+// play(chosenArray);
 });
-
-///PLAY GAME///////////////////////////////////////////////////
