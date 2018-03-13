99b972ba34488b1a7835b27cd0af10e96520b171
Jose Juan Parra Alcolea <josejuan.parraalcolea@192-168-1-8.tpgi.com.au>
2018-03-13 21:29:33 +1100

    add winner

---
+++ README.md
@@ -0,0 +1,58 @@
+# Project 0
+
+## Subheadings
+
+### Subsubheadings
+
+Some regular text
+
+
+Some regular text
+
+Somo *italic* or some **bold**
+
+Or ***both***
+
+This is a paragraph.
+
+## List
+
+* list item
+* second item
+
+### Marx Brothers
+
+1. Groucho
+1. Harpo
+1. Chico
+
+
+![Bill Murray](http://fillmurray.com/50/70)
+
+http://twiter.com
+
+[Twiter](http://twiter.com)
+
+
+To quote some famous:
+
+>Famous inspirational quote
+
+You should use the `<section>` tag.
+
+There are two many variables called `scored` in this project.
+
+## Github Flavoured Markdown
+
+### Function expresion example
+
+```
+const sayHello = function () {
+  console.log('Hello!');
+}
+```
+
+@coldhead      >tag people
+
+
+~~this is struck out~~ updated docs
--- js/main.js
+++ js/main.js
@@ -23,23 +23,112 @@ let winCombinations = [
 ];
 let playerTurn = '';
 let turn = 0;
-let chosenArray = [];
-
-
+// let chosenArray = [];
+// let chosenSquare;
 
 let playGame = function () {
-  parseInt(this.id);
-  console.log(this.id.parseInt());
+  let num = parseInt(this.id);
+  console.log(num);
+  indexNum(num)
+  winner();
+};
+
+
+
+
+const winner = function () {
+if (
+      (gameOutcome[0] === 'X' && gameOutcome[1] === 'X' && gameOutcome[2] === 'X')||
+      (gameOutcome[3] === 'X' && gameOutcome[4] === 'X' && gameOutcome[5] ==='X')||
+      (gameOutcome[6] === 'X' && gameOutcome[7] === 'X' && gameOutcome[8] ==='X')||
+      (gameOutcome[0] === 'X' && gameOutcome[4] === 'X' && gameOutcome[8] ==='X')||
+      (gameOutcome[0] === 'X' && gameOutcome[3] === 'X' && gameOutcome[6] ==='X')||
+      (gameOutcome[1] === 'X' && gameOutcome[4] === 'X' && gameOutcome[7] ==='X')||
+      (gameOutcome[2] === 'X' && gameOutcome[5] === 'X' && gameOutcome[8] ==='X')||
+      (gameOutcome[2] === 'X' && gameOutcome[4] === 'X' && gameOutcome[6] ==='X')
+) {
+  console.log('Player 1 win!');
+} else if (
+      (gameOutcome[0] === 'O' && gameOutcome[1] === 'O' && gameOutcome[2] === 'O')||
+      (gameOutcome[3] === 'O' && gameOutcome[4] === 'O' && gameOutcome[5] ==='O')||
+      (gameOutcome[6] === 'O' && gameOutcome[7] === 'O' && gameOutcome[8] ==='O')||
+      (gameOutcome[0] === 'O' && gameOutcome[4] === 'O' && gameOutcome[8] ==='O')||
+      (gameOutcome[0] === 'O' && gameOutcome[3] === 'O' && gameOutcome[6] ==='O')||
+      (gameOutcome[1] === 'O' && gameOutcome[4] === 'O' && gameOutcome[7] ==='O')||
+      (gameOutcome[2] === 'O' && gameOutcome[5] === 'O' && gameOutcome[8] ==='O')||
+      (gameOutcome[2] === 'O' && gameOutcome[4] === 'O' && gameOutcome[6] ==='O')
+) {
+  console.log('Player 2 win!');
 }
-$('.square').on('click', playGame);
+};
+
 
 
 
 // PLAYER TURN//////////////////////////////////////////
+function indexNum(num){
+
+  if (turn % 2 === 0) {
+    playerTurn = 'X';
+
+    console.log('Player 1');
+  } else if (turn % 2 !== 0) {
+    playerTurn = 'O';    //Player 1 goes
+
+    console.log('Player 2');
+  }
+
+////STORE THE INDEX IN THE ARRAY//////////////////
+
+  gameOutcome.splice(num, 1, playerTurn)
+  console.log(gameOutcome)
+  turn +=1
+}
+
+$('.square').on('click', playGame);
+
+
+//////CHECK FOR WINNER/////////////////////////////
+
+
+  for (let i = 0; i < winCombinations.length; i++) {
+    let winCombination = winCombinations[i];
+    for (let j = 0; j < winCombination.length; j++) {
+    console.log(winCombinations[i][j]);
+
+
+
+    // if (indexOf.gameOutcome === winCombinations[i][j]) {
+    //   console.log('urra')
+    }
+  };
+
+
+  //   let c1 = winCombinations[0];
+  //   let c2 = winCombinations[1];
+  //   let c3 = winCombinations[2];
+  //
+  // gameOutcome[c1] == gameOutcome[c2] && gameOutcome[c2] == gameOutcome[c3]
+
+
+
+//////RESET THE GAME WHEN SOMEONE PRESS RESTART//////////////////
+
+let restart = function () {
+  turn = 0;
+  gameOutcome = [ '_' , '_' , '_' , '_' , '_' , '_' , '_' , '_' , '_' ];
+};
+
+$('.restart').on('click',restart)
+
+
+
+});
+// PLAYER TURN//////////////////////////////////////////
 
 // const newTurn = function (turn) {
 //   if (turn % 2 === 0) {
-//     playerTurn = 'O';     //Player 2 goes
+//     playerTurn = 'O';
 //
 //     console.log('Player 2');
 //   } else if (turn % 2 !== 0) {
@@ -55,7 +144,7 @@ $('.square').on('click', playGame);
 // ////CHOOSE THE MOVEMENT/////////////////////////////////////////
 //
 // $("html").click(function () {    /////CLICK ANYWHERE ON THE SCREEN
-//   turn = newTurn(turn)
+  // turn = newTurn(turn)
 // });
 
 ////////////////////////////////////////////////////////TEST//////
@@ -78,4 +167,3 @@ $('.square').on('click', playGame);
 //   }
 // };
 // play(chosenArray);
-});
